afac783e7de2648488e9e93bbeeca4a9b23d2f09
Michael James <micrjames921@gmail.com>
2022-09-17 07:39:59 -0700

    refactored 'carousel' code

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,7 @@
-import { gameValues, gameFigures, gameBtnPrev, gameBtnNext } from "./gamePost_incs.js";
+import { gameValues, gameBtnPrev, gameBtnNext } from "./gamePostsCarousel/gamePost_incs.js";
+import { setGamePostValues } from "./gamePostsCarousel/setGamePosts.js";
 
 let gamePostValuesIndex = 0;
-
-const setGamePostValues = function(which) {
-   gameFigures.forEach((gameFigure, index) => {
-	  const gameFigureClasslist = gameFigure.classList;
-	  if(gameFigureClasslist.contains(`game-${gameValues[index].id}`)) {
-		 const gamePostEls = gameFigure.children;
-
-		 gamePostEls[0].setAttribute("src", gameValues[which]["image"]);
-		 gamePostEls[1].textContent = gameValues[which]["name"];
-	  }
-   });
-};
 
 setGamePostValues(gamePostValuesIndex);
 gameBtnPrev.disabled = true;
