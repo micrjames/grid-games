1870274fd6c6342b18b049ac8faa01a9642b16b3
Michael James <micrjames921@gmail.com>
2022-09-21 18:34:51 -0700

    implemented 'flag placement' functionality to game logic

--- src/js/minesweeper/game_incs.js
+++ src/js/minesweeper/game_incs.js
@@ -3,4 +3,7 @@ const numCols = 9;
 const minesweeperEl = document.querySelector(".game-minesweeper");
 const msBoard = document.querySelector(".ms-board");
 
-export { numRows, numCols, minesweeperEl, msBoard };
+const ms_minesDisplay = document.querySelector(".game-interface-mines-display");
+const ms_countdownDisplay = document.querySelector(".game-interface-countdown");
+
+export { numRows, numCols, minesweeperEl, msBoard, ms_minesDisplay, ms_countdownDisplay };
--- src/js/minesweeper/game_logic.js
+++ src/js/minesweeper/game_logic.js
@@ -1,6 +1,25 @@
-import { numRows, numCols, minesweeperEl, msBoard } from "./game_incs.js";
+import { numRows, numCols, minesweeperEl, msBoard, ms_minesDisplay, ms_countdownDisplay } from "./game_incs.js";
+
+let totalNumMines = 10;
+
+const setRandNumsArr = (numRandNums, gridSize, exceptionIndex) => {
+   let randNumsArr = [];
+   for(let i = 0; i <= numRandNums-1; i++) {
+	  const randNum = Math.floor(Math.random() * gridSize);
+	  if(randNumsArr.length) {
+		 if(randNumsArr.some(num => num == randNum || num == exceptionIndex)) {
+			i--;
+			continue;
+		 }
+	  }
+	  randNumsArr.push(randNum);
+   }
+   return randNumsArr;
+};
 
 const startMS = function() {
+   ms_minesDisplay.textContent = totalNumMines;
+   ms_countdownDisplay.textContent = "60";
    const MSCellFragment = document.createDocumentFragment();
    for(let i = 0; i < numRows * numCols; i++) {
        const MScell = document.createElement("div");
@@ -8,13 +27,7 @@ const startMS = function() {
 	   MScell.classList.add("covered");
 	   MSCellFragment.appendChild(MScell);
 
-	   MScell.addEventListener("click", function() {
-			 // this.classList.toggle("covered");
-			 // this.classList.toggle("uncovered");
-		     const icon = addIcon("fa-solid fa-flag");
-		     
-		     this.appendChild(icon);
-	   });
+	   MScell.addEventListener("click", clickCell); 
    }  
    msBoard.appendChild(MSCellFragment);
 }  
@@ -23,11 +36,33 @@ const addIcon = function(iconClasses) {
 	const icon = document.createElement("i");
 
     const classes = iconClasses.split(" ");
-    classes.forEach(item =>  {
+    classes.forEach(item => {
 	   icon.classList.add(item);
 	});
 
     return icon;
+};
+
+const clickCell = function(event) {
+	if(event.metaKey) { 
+	   if(totalNumMines > 0) {
+		  if(this.children.length) {
+			 this.removeChild(this.firstChild);
+			 totalNumMines++;
+		  } else {
+			 const icon = addIcon("fa-solid fa-flag");
+			 this.appendChild(icon);
+			 totalNumMines--;
+		  }
+		  if(totalNumMines < 10) totalNumMines = `0${totalNumMines}`;
+		  ms_minesDisplay.textContent = totalNumMines;
+	   }
+	} else {
+	   if(this.classList.contains("covered")) {
+		   this.classList.remove("covered");
+		   this.classList.add("uncovered");
+	   }
+	}
 };
 
 export { startMS };
