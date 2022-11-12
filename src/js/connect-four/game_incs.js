04d24c04ceb008ff5851742edfe33653585ae0d4
Michael James <micrjames921@gmail.com>
2022-11-11 20:17:28 -0700

    got working general layout

--- src/js/connect-four/game_incs.js
+++
@@ -1,9 +0,0 @@
-const cf_data = {
-    "numRows": 6,
-    "numCols": 7,
-    "yellow": true
-};
-const cf = document.querySelector(".game-connect-four");
-const cfBoard = cf.children[0];
-
-export { cf_data, cfBoard };
