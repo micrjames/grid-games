2d56a6dc513797cef3af93da4d1f89a927311788
Michael James <micrjames921@gmail.com>
2024-01-31 23:19:23 -0700

    rebuilt basic structure of site

--- src/js/incs.js
+++
@@ -1,7 +0,0 @@
-const body = document.body;
-const main = body.children.namedItem("main");
-const modal = main.children.namedItem("modal");
-
-const games = main.children.namedItem("games-grid");
-
-export { modal, games };
--- src/js/modal.js
+++
@@ -1,9 +0,0 @@
-import { modal } from "./incs.js";
-
-const modalHdr = modal.children.namedItem("modal-header");
-const modalHdrCloseBtn = modalHdr.children.namedItem("modal-header-close");
-const modalHdrText = modalHdr.children.namedItem("modal-header-text");
-
-const modalBody = modal.children.namedItem("modal-body");
-
-export { modalHdrText, modalHdrCloseBtn, modalBody };
