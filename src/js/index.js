1ab8cb858233723c26e5d95fda4187d3093a2af7
Michael James <micrjames921@gmail.com>
2024-02-03 16:27:51 -0700

    got working piece placement and turn swapping

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 requirejs(['js/figure.min.js', 'js/modal.min.js', 'js/incs.js', 'js/utils/utils.js', 'js/utils/domHelpers.js'], function() {
+   const CLASS = {
+	  X: "x",
+	  CIRCLE: "circle"
+   };
    const span = createSpan("ellipsis", "Coming Soon");
    for(const figure of figures) {
 	  new Figure(figure, text => {
@@ -13,6 +17,25 @@ requirejs(['js/figure.min.js', 'js/modal.min.js', 'js/incs.js', 'js/utils/utils.
 			   game.classList.remove("hidden");
 			   if(span.parentElement === modal.body)
 				  modal.body.removeChild(span);
+			   /*********/
+			   let circleTurn = true;
+			   const board = game.children.namedItem("board");
+			   board.classList.add("circle");
+			   const cellEls = board.children;
+			   for(const cellEl of cellEls) {
+				  cellEl.addEventListener("click", () => {
+					 const currentClass = circleTurn ? CLASS.CIRCLE : CLASS.X;
+					 cellEl.classList.add(currentClass);
+
+					 circleTurn = !circleTurn;
+					 board.classList.remove(CLASS.X);
+					 board.classList.remove(CLASS.CIRCLE);
+
+					 if(circleTurn) board.classList.add(CLASS.CIRCLE);
+					 else board.classList.add(CLASS.X);
+				  });
+			   }
+			   /*********/
 			} else {
 			   game.classList.add("hidden");
 			   modal.body.appendChild(span);
