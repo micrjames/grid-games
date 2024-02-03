b5a4d5027bc4eaba849e7461787c79e34bc24dd8
Michael James <micrjames921@gmail.com>
2024-02-02 19:17:30 -0700

    added filler text to modal body and created an animated ellipsis on text

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
+const modalBodyTxt = document.createTextNode("Coming Soon");
+const span = document.createElement("span");
+span.setAttribute("class", "ellipsis");
+span.appendChild(modalBodyTxt);
 requirejs(['js/figure.min.js', 'js/modal.min.js', 'js/incs.js'], function() {
    for(const figure of figures) {
 	  new Figure(figure, text => {
 		 const modal = new Modal(modalEl);
-		 modal.hdrText = text;
-		 modal.open();
 		 modal.doOnClose();
+		 modal.hdrText = text;
+		 modal.body.appendChild(span);
+		 modal.open();
 		 window.scrollTo(0, 0);
 	  });
    }
