75520edc83bdf97a8fd3d625fc309bdcff5d0f31
Michael James <micrjames921@gmail.com>
2024-02-04 12:14:04 -0700

    refactored TTT class

--- src/js/ts/TTT.js
+++ src/js/ts/TTT.js
@@ -2,45 +2,107 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TTT = void 0;
 var Cell = /** @class */ (function () {
-    function Cell(cellEl, cb) {
-        this.cell = cellEl;
-        this.cell.addEventListener("click", function () {
-            cb();
-        });
+    function Cell(cellEl) {
+        this._cell = cellEl;
     }
+    Cell.prototype.handleClick = function (cb) {
+        this._cell.addEventListener("click", function () {
+            cb();
+        }, { once: true });
+    };
+    Object.defineProperty(Cell.prototype, "cell", {
+        get: function () {
+            return this._cell;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Cell;
 }());
 var TTT = /** @class */ (function () {
     function TTT(board) {
+        this.cells = [];
         this.circleTurn = true;
+        this.gameOver = false;
         this._board = board;
         this.cellEls = board.children;
+        for (var i = 0; i < this.cellEls.length; i++) {
+            this.cells.push(new Cell(this.cellEls[i]));
+        }
         this.CLASS = {
             X: "x",
             CIRCLE: "circle"
         };
-        this._board.classList.add("circle");
+        this.start();
     }
     TTT.prototype.setMarks = function () {
         var _this = this;
         var _loop_1 = function (i) {
-            var cell = new Cell(this_1.cellEls[i], function () {
+            this_1.cells[i].handleClick(function () {
                 var currentClass = _this.circleTurn ? _this.CLASS.CIRCLE : _this.CLASS.X;
-                _this.cellEls[i].classList.add(currentClass);
-                _this.circleTurn = !_this.circleTurn;
-                _this._board.classList.remove(_this.CLASS.X);
-                _this._board.classList.remove(_this.CLASS.CIRCLE);
-                if (_this.circleTurn)
-                    _this._board.classList.add(_this.CLASS.CIRCLE);
-                else
-                    _this._board.classList.add(_this.CLASS.X);
+                // const pos = getPos(cell);
+                if (!_this.gameOver)
+                    _this.setMark(i, currentClass);
+                // if(checkWin(mat, currentClass, pos)) {
+                // 	const titledClass = titleClass(currentClass));
+                // 	gameOver = endGame(`${titledClass} wins!`, gameOver);
+                //  } else if(checkDraw(mat))
+                //  gameOver = endGame("There is a draw!", gameover);
+                if (!_this.gameOver) {
+                    _this.circleTurn = !_this.circleTurn;
+                    _this.setBoardHoverClass();
+                }
             });
         };
         var this_1 = this;
-        for (var i = 0; i < this.cellEls.length; i++) {
+        for (var i = 0; i < this.cells.length; i++) {
             _loop_1(i);
         }
     };
+    TTT.prototype.setMark = function (which, className) {
+        switch (className) {
+            case this.CLASS.CIRCLE:
+                // mat.circle.setElement(1, pos.row, pos.col);
+                break;
+            case this.CLASS.X:
+                // mat.x.setElement(1, pos.row, pos.col);
+                break;
+        }
+        this.cells[which].cell.classList.add(className);
+    };
+    TTT.prototype.setup = function () {
+        for (var i = 0; i < this.cells.length; i++) {
+            if (this.cells[i].cell.classList.contains(this.CLASS.X))
+                this.cells[i].cell.classList.remove(this.CLASS.X);
+            else if (this.cells[i].cell.classList.contains(this.CLASS.CIRCLE))
+                this.cells[i].cell.classList.remove(this.CLASS.CIRCLE);
+        }
+        // toggleUIEls(false);
+    };
+    TTT.prototype.setBoardHoverClass = function () {
+        this._board.classList.remove(this.CLASS.X);
+        this._board.classList.remove(this.CLASS.CIRCLE);
+        if (this.circleTurn)
+            this._board.classList.add(this.CLASS.CIRCLE);
+        else
+            this._board.classList.add(this.CLASS.X);
+    };
+    TTT.prototype.start = function () {
+        this.setup();
+        this.setBoardHoverClass();
+    };
+    TTT.prototype.reset = function () {
+        this.setup();
+        // mat.circle.clear();
+        // mat.x.clear();
+        this.gameOver = false;
+    };
+    TTT.prototype.endGame = function (msg, gameOver) {
+        // gameOver = true;
+        // winningMessageText.textContent = msg;
+        // toggleUIEls(gameOver);
+    };
+    ;
     return TTT;
 }());
 exports.TTT = TTT;
--- src/js/ts/TTT.ts
+++ src/js/ts/TTT.ts
@@ -1,11 +1,16 @@
 class Cell {   
-    private cell: Element;  
-    constructor(cellEl: Element, cb: () => void) {                      
-	   this.cell = cellEl;   
-       this.cell.addEventListener("click", () => {   
-		  cb();
-       });
+    private _cell: Element;  
+    constructor(cellEl: Element) {                      
+	   this._cell = cellEl;   
     }
+	handleClick(cb: () => void) {
+       this._cell.addEventListener("click", () => {   
+		  cb();
+       }, { once: true });
+	}
+	get cell(): Element {
+	   return this._cell;
+	}
 } 
 
 interface CLASSES {
@@ -14,33 +19,98 @@ interface CLASSES {
 
 export class TTT {
    private circleTurn: boolean;
+   private gameOver: boolean;
    private _board: HTMLElement;
    private cellEls: HTMLCollection;
+   private cells: Cell[] = [];
    private CLASS: CLASSES;
 
    constructor(board: HTMLElement) {
 	  this.circleTurn = true;
+	  this.gameOver = false;
 	  this._board = board;
 	  this.cellEls = board.children;
+	  for(let i = 0; i < this.cellEls.length; i++) {
+	  	  this.cells.push(new Cell(this.cellEls[i]));
+	  }
 	  this.CLASS = {
 		  X: "x",
 		  CIRCLE: "circle"
 	  };
-	  this._board.classList.add("circle");
+
+	  this.start();
    }
 
    setMarks() {
-	  for(let i = 0; i < this.cellEls.length; i++) {
-		 const cell = new Cell(this.cellEls[i], () => {
+	  for(let i = 0; i < this.cells.length; i++) {
+		 this.cells[i].handleClick(() => {
 			const currentClass = this.circleTurn ? this.CLASS.CIRCLE : this.CLASS.X;
-			this.cellEls[i].classList.add(currentClass);
-			this.circleTurn = !this.circleTurn;
-			this._board.classList.remove(this.CLASS.X);
-			this._board.classList.remove(this.CLASS.CIRCLE);
-
-			if(this.circleTurn) this._board.classList.add(this.CLASS.CIRCLE);
-			else this._board.classList.add(this.CLASS.X);
+			// const pos = getPos(cell);
+			if(!this.gameOver) this.setMark(i, currentClass);
+			// if(checkWin(mat, currentClass, pos)) {
+			// 	const titledClass = titleClass(currentClass));
+			// 	gameOver = endGame(`${titledClass} wins!`, gameOver);
+			//  } else if(checkDraw(mat))
+			//  gameOver = endGame("There is a draw!", gameover);
+			if(!this.gameOver) {
+			   this.circleTurn = !this.circleTurn;
+			   this.setBoardHoverClass();
+			}
 		 });
 	  }
    }
+   setMark(which: number, className: string) {
+	  switch(className) {
+		   case this.CLASS.CIRCLE:
+			  // mat.circle.setElement(1, pos.row, pos.col);
+		   break;
+		   case this.CLASS.X:
+			  // mat.x.setElement(1, pos.row, pos.col);
+           break;
+	  }
+	  this.cells[which].cell.classList.add(className);
+   }
+
+   setup() {
+	  for(let i = 0; i < this.cells.length; i++) {
+	    if(this.cells[i].cell.classList.contains(this.CLASS.X)) this.cells[i].cell.classList.remove(this.CLASS.X);
+	    else if(this.cells[i].cell.classList.contains(this.CLASS.CIRCLE)) this.cells[i].cell.classList.remove(this.CLASS.CIRCLE);
+	  }
+
+	  // toggleUIEls(false);
+   }
+   setBoardHoverClass() {
+	  this._board.classList.remove(this.CLASS.X);
+	  this._board.classList.remove(this.CLASS.CIRCLE);
+
+	  if(this.circleTurn) this._board.classList.add(this.CLASS.CIRCLE);
+	  else this._board.classList.add(this.CLASS.X);
+   }
+   start() {
+	  this.setup();
+	  this.setBoardHoverClass();
+   }
+   reset() {
+	  this.setup();
+
+	  // mat.circle.clear();
+	  // mat.x.clear();
+
+	  this.gameOver = false;
+   }
+
+   endGame(msg: string, gameOver: boolean) {
+	   // gameOver = true;
+	   // winningMessageText.textContent = msg;
+	   // toggleUIEls(gameOver);
+   };
+/*
+   const toggleUIEls = function(gameDone) {
+	   if(gameDone) {
+		   changeSectionVisibility(winningMessageEl, instructionsMessageEl);
+	   } else {
+		   changeSectionVisibility(instructionsMessageEl, winningMessageEl);
+	   }
+   }
+*/
 }
