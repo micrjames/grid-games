c4f7507a93b1842ef40bb26ef6cb5091d822608b
Michael James <micrjames921@gmail.com>
2024-02-05 02:43:42 -0700

    got matrix code working with TTT class so that marks are recorded in the matrices

--- src/js/ts/TTT.js
+++ src/js/ts/TTT.js
@@ -1,15 +1,186 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TTT = void 0;
+var Matrix = /** @class */ (function () {
+    function Matrix(N) {
+        this.N = N;
+        this.clear();
+    }
+    Matrix.fromArray = function (array) {
+        var N = array.length;
+        var arrMat = new Matrix(N);
+        arrMat._mat = array;
+        return arrMat;
+    };
+    Matrix.prototype.set = function () {
+        for (var i = 0; i < this.N; i++) {
+            this._mat = __spreadArray(__spreadArray([], this._mat, true), [Array(this.N).fill(0)], false);
+        }
+    };
+    Matrix.prototype.clear = function () {
+        this._mat = [];
+        this.set();
+    };
+    Object.defineProperty(Matrix.prototype, "size", {
+        get: function () {
+            return this.N;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Matrix.prototype, "mat", {
+        get: function () {
+            return this._mat;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Matrix.prototype.setElement = function (val, j, i) {
+        this._mat[j][i] = val;
+    };
+    Matrix.prototype.getElement = function (j, i) {
+        return this._mat[j][i];
+    };
+    Matrix.prototype.setRow = function (val, which) {
+        this._mat[which] = val;
+    };
+    Matrix.prototype.getRow = function (which) {
+        return this._mat.filter(function (_, index) { return index == which; }).flat();
+    };
+    Matrix.prototype.getCol = function (which) {
+        return this._mat.map(function (row) { return row[which]; });
+    };
+    Matrix.prototype.getDiagonal = function (k) {
+        var diagonal = [];
+        var pivot = Math.abs(k);
+        if (k >= 0) {
+            for (var rowIndex = 0; rowIndex < this.N; rowIndex++) {
+                for (var colIndex = 0; colIndex < this.N; colIndex++) {
+                    if (colIndex == pivot) {
+                        pivot++;
+                        diagonal = __spreadArray(__spreadArray([], diagonal, true), [this._mat[rowIndex][colIndex]], false);
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            for (var colIndex = 0; colIndex < this.N; colIndex++) {
+                for (var rowIndex = 0; rowIndex < this.N; rowIndex++) {
+                    if (rowIndex == pivot) {
+                        pivot++;
+                        diagonal = __spreadArray(__spreadArray([], diagonal, true), [this._mat[rowIndex][colIndex]], false);
+                        break;
+                    }
+                }
+            }
+        }
+        return diagonal;
+    };
+    Matrix.prototype.getCounterDiagonal = function (k) {
+        var counterMatrix = new Matrix(this.N);
+        this._mat.forEach(function (row, index) {
+            var revRow = row.reduce(function (acc, item) { return [item].concat(acc); }, []);
+            counterMatrix.setRow(revRow, index);
+        });
+        var reverseCounterDiagonal = counterMatrix.getDiagonal(k);
+        var counterDiagonal = reverseCounterDiagonal.reverse();
+        return counterDiagonal;
+    };
+    Object.defineProperty(Matrix.prototype, "main_diagonal", {
+        get: function () {
+            return this.getDiagonal(0);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Matrix.prototype, "main_counterDiagonal", {
+        get: function () {
+            return this.getCounterDiagonal(0);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Matrix.prototype.transpose = function () {
+        var tMat = new Matrix(this.N);
+        for (var j = 0; j < this.N; j++) {
+            for (var i = 0; i < this.N; i++) {
+                tMat.mat[j][i] = this._mat[i][j];
+            }
+        }
+        return tMat;
+    };
+    Matrix.prototype.add = function (thatMat) {
+        var addedArray = this._mat.map(function (row, rowIndex) {
+            return row.map(function (el, elIndex) {
+                return el + thatMat.mat[rowIndex][elIndex];
+            });
+        });
+        return Matrix.fromArray(addedArray);
+    };
+    Matrix.prototype.multiply = function (thatMat) {
+        var _this = this;
+        var multArray = this._mat.map(function (row, rowIndex) { return row.map(function (_, colIndex) {
+            var row = _this.getRow(rowIndex);
+            var col = thatMat.getCol(colIndex);
+            return row.reduce(function (accumulator, currentValue, currentIndex) {
+                return currentValue * col[currentIndex] + accumulator;
+            }, 0);
+        }); });
+        return Matrix.fromArray(multArray);
+    };
+    Matrix.prototype.multiply_scalar = function (scalar) {
+        return Matrix.fromArray(this._mat.map(function (row) { return row.map(function (el) { return scalar * el; }); }));
+    };
+    Matrix.prototype.toString = function () {
+        var _this = this;
+        var mstring = '';
+        this._mat.forEach(function (row, rowIndex) {
+            mstring += '[\t';
+            row.forEach(function (_, colIndex) {
+                mstring += _this._mat[rowIndex][colIndex] + '\t';
+            });
+            mstring += ']\n';
+        });
+        return mstring;
+    };
+    return Matrix;
+}());
 var Cell = /** @class */ (function () {
     function Cell(cellEl) {
         this._cell = cellEl;
+        this._pos = {
+            row: -1,
+            col: -1
+        };
     }
     Cell.prototype.handleClick = function (cb) {
         this._cell.addEventListener("click", function () {
             cb();
         }, { once: true });
     };
+    Object.defineProperty(Cell.prototype, "pos", {
+        get: function () {
+            var pos = this._cell.dataset.pos;
+            var splitPos = pos.split("-");
+            this._pos = { row: +splitPos[0], col: +splitPos[1] };
+            return this._pos;
+        },
+        set: function (pos) {
+            this._pos = pos;
+        },
+        enumerable: false,
+        configurable: true
+    });
     Object.defineProperty(Cell.prototype, "cell", {
         get: function () {
             return this._cell;
@@ -33,6 +204,10 @@ var TTT = /** @class */ (function () {
             X: "x",
             CIRCLE: "circle"
         };
+        this.MAT = {
+            X: new Matrix(3),
+            CIRCLE: new Matrix(3)
+        };
         this.start();
     }
     TTT.prototype.setMarks = function () {
@@ -40,7 +215,8 @@ var TTT = /** @class */ (function () {
         var _loop_1 = function (i) {
             this_1.cells[i].handleClick(function () {
                 var currentClass = _this.circleTurn ? _this.CLASS.CIRCLE : _this.CLASS.X;
-                // const pos = getPos(cell);
+                _this.cells[i].pos = _this.cells[i].pos;
+                console.log(_this.cells[i].pos);
                 if (!_this.gameOver)
                     _this.setMark(i, currentClass);
                 // if(checkWin(mat, currentClass, pos)) {
@@ -62,12 +238,14 @@ var TTT = /** @class */ (function () {
     TTT.prototype.setMark = function (which, className) {
         switch (className) {
             case this.CLASS.CIRCLE:
-                // mat.circle.setElement(1, pos.row, pos.col);
+                this.MAT.CIRCLE.setElement(1, this.cells[which].pos.row, this.cells[which].pos.col);
                 break;
             case this.CLASS.X:
-                // mat.x.setElement(1, pos.row, pos.col);
+                this.MAT.X.setElement(1, this.cells[which].pos.row, this.cells[which].pos.col);
                 break;
         }
+        console.log(this.MAT.X.toString());
+        console.log(this.MAT.CIRCLE.toString());
         this.cells[which].cell.classList.add(className);
     };
     TTT.prototype.setup = function () {
--- src/js/ts/TTT.ts
+++ src/js/ts/TTT.ts
@@ -1,12 +1,177 @@
+class Matrix {
+   private _mat: number[][];
+   private N: number;
+
+   constructor(N: number) {
+	   this.N = N;
+	   this.clear();
+   }
+
+   static fromArray(array: number[][]): Matrix {
+	   const N = array.length;
+	   const arrMat = new Matrix(N);
+	   arrMat._mat = array;
+
+	   return arrMat;
+   }
+
+   private set() {
+	   for(let i = 0; i < this.N; i++) {
+		   this._mat = [...this._mat, Array(this.N).fill(0)];
+	   }
+   }
+
+   clear() {
+	   this._mat = [];
+	   this.set();
+   }
+
+   get size(): number {
+	   return this.N;
+   }
+
+   get mat()  {
+	   return this._mat;
+   }
+
+   setElement(val: number, j: number, i: number) {
+	  this._mat[j][i] = val;
+   }
+   getElement(j: number, i: number): number {
+	  return this._mat[j][i];
+   }
+
+   setRow(val: any[], which: number) {
+	  this._mat[which] = val;
+   }
+   getRow(which: number): any[] {
+	  return this._mat.filter((_, index) => index == which).flat();
+   }
+   getCol(which: number): any[] {
+	  return this._mat.map(row => row[which]);
+   }
+
+   getDiagonal(k: number) {
+	   let diagonal = [];
+	   let pivot = Math.abs(k);
+	   if (k >= 0) {
+		  for(let rowIndex = 0; rowIndex < this.N; rowIndex++) {
+			  for(let colIndex = 0; colIndex < this.N; colIndex++) {
+					if(colIndex == pivot) {
+					   pivot++;
+					   diagonal = [...diagonal, this._mat[rowIndex][colIndex]];
+
+					   break;
+					}
+			  }
+		  }
+	   } else {
+		  for(let colIndex = 0; colIndex < this.N; colIndex++) {
+			  for(let rowIndex = 0; rowIndex < this.N; rowIndex++) {
+					if(rowIndex == pivot) {
+					   pivot++;
+					   diagonal = [...diagonal, this._mat[rowIndex][colIndex]];
+
+					   break;
+					}
+			  }
+		  }
+	   }
+
+	   return diagonal;
+   }
+   getCounterDiagonal(k: number) {
+	   const counterMatrix = new Matrix(this.N);
+	   this._mat.forEach((row, index) => {
+		   const revRow = row.reduce((acc, item) => [item].concat(acc), []);
+		   counterMatrix.setRow(revRow, index);
+	   });
+
+	   const reverseCounterDiagonal = counterMatrix.getDiagonal(k);
+	   const counterDiagonal = reverseCounterDiagonal.reverse();
+	   return counterDiagonal;
+   }
+   get main_diagonal() {
+	   return this.getDiagonal(0);
+   }
+   get main_counterDiagonal() {
+	   return this.getCounterDiagonal(0);
+   }
+
+   transpose(): Matrix {
+	  const tMat = new Matrix(this.N);
+
+	  for(let j = 0; j < this.N; j++) {
+		  for(let i = 0; i < this.N; i++) {
+			  tMat.mat[j][i] = this._mat[i][j];
+		  }
+	  }
+	  return tMat;
+   }
+
+   add(thatMat: Matrix): Matrix {
+	  const addedArray = this._mat.map((row, rowIndex) => {
+		   return row.map((el, elIndex) => {
+			   return el + thatMat.mat[rowIndex][elIndex];
+		   });
+	   });
+	  return Matrix.fromArray(addedArray);
+   }
+   multiply(thatMat: Matrix): Matrix {
+	  const multArray = this._mat.map((row, rowIndex) => row.map((_, colIndex) => {
+		  const row = this.getRow(rowIndex);
+		  const col = thatMat.getCol(colIndex);
+		  return row.reduce((accumulator, currentValue, currentIndex) => {
+			 return currentValue * col[currentIndex] + accumulator;
+		  }, 0);
+	  })); 
+	  return Matrix.fromArray(multArray);
+   }
+   multiply_scalar(scalar: number): Matrix {
+	  return Matrix.fromArray(this._mat.map(row => row.map(el => scalar * el)));
+   }
+
+   toString(): string {
+	  let mstring = '';
+
+	  this._mat.forEach((row, rowIndex) => {
+		mstring += '[\t';
+		row.forEach((_, colIndex) => {
+		   mstring += this._mat[rowIndex][colIndex] + '\t';
+		});
+		mstring += ']\n';
+	  });
+
+	  return mstring;
+   }
+}
+
+interface POS {
+	[prop: string]: number
+}
 class Cell {   
-    private _cell: Element;  
-    constructor(cellEl: Element) {                      
-	   this._cell = cellEl;   
+    private _cell: HTMLElement;  
+	private _pos: POS;
+    constructor(cellEl: HTMLElement) {                      
+	   this._cell = cellEl;
+	   this._pos = {
+			row: -1,
+			col: -1
+	   }; 
     }
 	handleClick(cb: () => void) {
        this._cell.addEventListener("click", () => {   
 		  cb();
        }, { once: true });
+	}
+	get pos(): POS {
+		const pos = this._cell.dataset.pos;
+		const splitPos = pos.split("-");
+		this._pos = { row: +splitPos[0], col: +splitPos[1] };
+		return this._pos;
+	}
+	set pos(pos: POS) {
+		this._pos = pos;
 	}
 	get cell(): Element {
 	   return this._cell;
@@ -14,7 +179,10 @@ class Cell {
 } 
 
 interface CLASSES {
- [prop: string]: string;
+   [prop: string]: string;
+}
+interface MATS {
+   [prop: string]: Matrix;
 }
 
 export class TTT {
@@ -24,6 +192,7 @@ export class TTT {
    private cellEls: HTMLCollection;
    private cells: Cell[] = [];
    private CLASS: CLASSES;
+   private MAT: MATS;
 
    constructor(board: HTMLElement) {
 	  this.circleTurn = true;
@@ -31,11 +200,15 @@ export class TTT {
 	  this._board = board;
 	  this.cellEls = board.children;
 	  for(let i = 0; i < this.cellEls.length; i++) {
-	  	  this.cells.push(new Cell(this.cellEls[i]));
+	  	  this.cells.push(new Cell(this.cellEls[i] as HTMLElement));
 	  }
 	  this.CLASS = {
 		  X: "x",
 		  CIRCLE: "circle"
+	  };
+	  this.MAT = {
+		 X: new Matrix(3),
+		 CIRCLE: new Matrix(3)
 	  };
 
 	  this.start();
@@ -45,7 +218,8 @@ export class TTT {
 	  for(let i = 0; i < this.cells.length; i++) {
 		 this.cells[i].handleClick(() => {
 			const currentClass = this.circleTurn ? this.CLASS.CIRCLE : this.CLASS.X;
-			// const pos = getPos(cell);
+			this.cells[i].pos = this.cells[i].pos;
+			console.log(this.cells[i].pos)
 			if(!this.gameOver) this.setMark(i, currentClass);
 			// if(checkWin(mat, currentClass, pos)) {
 			// 	const titledClass = titleClass(currentClass));
@@ -62,12 +236,14 @@ export class TTT {
    setMark(which: number, className: string) {
 	  switch(className) {
 		   case this.CLASS.CIRCLE:
-			  // mat.circle.setElement(1, pos.row, pos.col);
+			  this.MAT.CIRCLE.setElement(1, this.cells[which].pos.row, this.cells[which].pos.col);
 		   break;
 		   case this.CLASS.X:
-			  // mat.x.setElement(1, pos.row, pos.col);
+			  this.MAT.X.setElement(1, this.cells[which].pos.row, this.cells[which].pos.col);
            break;
 	  }
+	  console.log(this.MAT.X.toString());
+	  console.log(this.MAT.CIRCLE.toString());
 	  this.cells[which].cell.classList.add(className);
    }
 
